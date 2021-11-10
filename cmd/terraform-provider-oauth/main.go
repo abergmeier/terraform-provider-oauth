fc684bc50b6cbe0603fbdb2b535a1c63200f7039
Andreas Bergmeier <andreas.bergmeier@otto.de>
2021-11-10 14:25:29 +0100

    Remove timestamp from log output
    Timestamps are not necessary when running from Terraform.

--- cmd/terraform-provider-oauth/main.go
+++ cmd/terraform-provider-oauth/main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"log"
+
 	"github.com/abergmeier/terraform-provider-oauth/internal/provider"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 )
 
 func main() {
+
+	// Since terraform catches our output
+	// no need for us to have timestamp
+	log.SetFlags(0)
 
 	plugin.Serve(&plugin.ServeOpts{
 		ProviderFunc: provider.Provider,
